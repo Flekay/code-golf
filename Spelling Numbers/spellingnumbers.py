dd76d5d687521982cfac01d94f5458726217a202
flekay <flekay@hotmail.de>
2025-07-10 17:35:58 +0200

    improve spelling function to 399 bytes

--- Spelling Numbers/spellingnumbers.py
+++ Spelling Numbers/spellingnumbers.py
@@ -1,13 +1,4 @@
 import sys
-def spell(n):
- words = ["zero","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"]
- word2 = [0,0,"twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"]
- if n>999:return "one thousand"
- if n > 99:
-  i = n%100
-  return words[n//100] + " hundred" + (" and " + spell(n%100)if i>0else"")
- if n > 19:
-  i = n%10
-  return word2[n//10] + ("-"+words[i]if i>0else"")
- return words[n]
-for n in sys.argv[1:]:print(spell(int(n)))
+w="zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen twenty thirty forty fifty sixty seventy eighty ninety".split()
+s=lambda n:n>999and"one thousand"or n>99and w[n//100]+" hundred"+(" and "+s(n%100)if n%100else"")or n>19and w[n//10+18]+("-"+w[n%10]if n%10else"")or w[n]
+for x in sys.argv[1:]:print(s(int(x)))
