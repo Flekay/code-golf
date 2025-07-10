7cbc952141389bcdee75bd9d2ffbea2bad6fa8a2
flekay <flekay@hotmail.de>
2025-07-10 22:13:46 +0200

    imporove Foo Fizz Buzz Bar to 90 bytes

---
+++ Foo Fizz Buzz Bar/foofizzbuzzbar.py
@@ -0,0 +1,2 @@
+i=1
+while i<1001:print('Foo'[i%2*4:]+'Fizz'[i%3*5:]+'Buzz'[i%5*5:]+'Bar'[i%7*4:]or i);i+=1
--- FooFizzBuzzBar/foofizzbuzzbar.py
+++
@@ -1,1 +0,0 @@
-for i in range(1,1001):print('Foo'[i%2*4:]+'Fizz'[i%3*5:]+'Buzz'[i%5*5:]+'Bar'[i%7*4:]or i)
