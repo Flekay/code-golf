643b06b968e9554a32a98f3a8c73b6450e06ee6c
flekay <flekay@hotmail.de>
2025-07-11 02:05:42 +0200

    improve Transpose Sentence to 117 bytes

--- Transpose Sentence/python.py
+++ Transpose Sentence/python.py
@@ -1,7 +1,2 @@
 import sys
-for a in sys.argv[1:]:
- s=a.split();o=''
- for i in range(len(max(s,key=len))):
-  for x in s:o+=''if len(x)-1<i else x[i]
-  o+=' '
- print(o)
+for a in sys.argv[1:]:s=a.split();print(*[''.join(x[i:i+1]for x in s)for i in range(len(max(s,key=len)))])
