43f9e34bccac715d5d71f0ccc070804423bea8a3
flekay <flekay@hotmail.de>
2025-08-09 01:57:36 +0200

    add Kaprekar numbers implementation in Python

---
+++ kaprekar-numbers/python.py
@@ -0,0 +1,2 @@
+s=0
+for x in'1,8,10,a,18,5i,ba,88,y0,e1,1nr,1z,2s,6q,1j0,e1,1pq,5o1,3ri,cx0,ty8,3ri,9m9,3ri,x2i,430,pza,42z,gi1,2d0r,8ph,gi1,42z,pza,1iyi,42z,p7i,rs,p7i,42z,1iyi,du0,c5a,42z,gi1,8ph,2d0r,gi1,42z,pza,430,285z,pza,42z,21tr1,ad2h,337a,8qci,1mpz,1jv2s,py09,dg9q,ntc9,1i8cs,1r7m8,1lhsr,apua,2yq4q'.split(','):s+=int(x,36);print(s)
