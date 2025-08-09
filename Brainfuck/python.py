5f1ca06b261d0da309f6db58b206bd1afec9814d
flekay <flekay@hotmail.de>
2025-08-09 05:06:04 +0200

    refactor Number Spiral implementation for improved readability and efficiency

--- Brainfuck/python.py
+++ Brainfuck/python.py
@@ -1,5 +1,5 @@
 import sys,os
 for s in sys.argv[1:]:
- P=i='';a,j=[0]*30000,0
- for c in s:x="><+-.[".find(c);P+=i+("j+=1","j-=1","a[j]+=1","a[j]-=1","os.write(1,bytes([a[j]%256]))","while a[j]:","")[x]+"\n";i=i[c==']':]+' '[c!='[':]
+ P=i='';a,j=[0]*9**5,0
+ for c in s:x="><+-.[".find(c);P+=i+("j+=1","j-=1","a[j]+=1","a[j]-=1","os.write(1,bytes([a[j]&255]))","while a[j]:","")[x]+"\n";i=i[c==']':]+' '[c!='[':]
  exec(P)
--- Number Spiral/python.py
+++ Number Spiral/python.py
@@ -1,8 +1,3 @@
-x=y=e=0
-t=[[-1]*10for _ in[0]*10]
-d=[(0,1),(1,0),(0,-1),(-1,0)]
-for i in range(100):
- t[x][y]=i;a,b=d[e];w,z=x+a,y+b
- if not(0<=w<10>z>=0>t[w][z]):a,b=d[e:=(e+1)%4];w,z=x+a,y+b
- x,y=w,z
-for r in t:print(*[f"{n:2}"for n in r])
+n=10;a=[[-1]*n for _ in[0]*n];x=y=d=i=0;p=0,1,0,-1,0
+while i<n*n:a[x][y]=i;d=d if 0<=x+p[d]<n>y+p[d+1]>=0>a[x+p[d]][y+p[d+1]]else(d+1)%4;x+=p[d];y+=p[d+1];i+=1
+for r in a:print(*map('{:2}'.format,r))
