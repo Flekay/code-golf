4cf86161adc977996859a76295dab1f31b0da193
Flekay <flekay@hotmail.de>
2026-02-12 18:51:40 +0100

    another one

--- Abundant Numbers (Long)/python.py
+++ Abundant Numbers (Long)/python.py
@@ -1,1 +1,1 @@
-[print(i)for i in range(1001)if sum(j*(i%j<1)for j in range(1,i))>i]
+for i in range(1001):sum(j*(i%j<1)for j in range(1,i))>i==print(i)
--- Catalan Numbers/python.py
+++ Catalan Numbers/python.py
@@ -1,2 +1,2 @@
-c=1
-for n in range(100):print(c);c*=4*n+2;c//=n+2
+c=n=1
+while n<101:print(c);c*=4*n-2;c//=n+1;n+=1
