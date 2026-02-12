7a2e21b688687eae269c155f4d8c1bffd74088f0
Flekay <flekay@hotmail.de>
2026-02-12 08:20:31 +0100

    Catalan’s Constant improvement

--- Catalan's Constant/python.py
+++ Catalan's Constant/python.py
@@ -1,4 +1,6 @@
-from string import*
-r=0
-for c in"}S884(<rH,&4b%c#fm?6vK6|:2]GQ~xFGDK*@$Cc0X5:,l8g;__WYPLP%pQ#[b1kZ+];i#6~I_JW%I$%ETEPT^S-}IFq=on{i6)1^riQ1'xD(Q-dK'o_}SID+nUFCc$b2bO_{wF sy';UwO4/X|,)?&/N?0`z^ZFM]uQm+hKdN4p=~uVh'WVI*[S'8|>F%U{}dU'S|%m13s@46KD`RRs9F=K$8TFD.RvTCEn^Idcbx7eD?|Ym|HHEx5SRrdzzd@6:M;:cqZFCjSe?REjv-OF+QwZPR}L]oduIrf1WIC^C~c^3C!Hps{%7rz`tW-ZFZe,Tip7i[V7?3NLGIr/KGEKWF3Q}5fXfyq~q@?,#/~g^&3^Irq7L!Rh'L{bf8Gk_/O<Cu;_6HG_9YOS(Nag>ZRhM7Yz5%g&Z2pS(eZqDx}+uycK<?^O)vNT/_U#[pF</4;IT(m+N(WM%/z_QRC24UrZG2fx'=l|f%j%:kt[x'js92~rR}Y}sH'tM&vB^uE^":r=r*93+{c:i for i,c in enumerate(digits+ascii_uppercase+ascii_lowercase+"!#$%&'()*+-.:;<=>?@[]^_{|}~`,/ ")}[c]
-print(f"0.{r}")
+from decimal import*
+getcontext().prec=n=1305
+d=(3+Decimal(8).sqrt())**n/2
+b,c,s=-d**0,-d,0
+for k in range(n):j=k-~k;c=b-c;s+=c/j/j;b=b*2*(k+n)*(k-n)/j/-~k
+print(str(s/d)[:1002])
