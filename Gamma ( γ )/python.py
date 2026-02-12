e9b255d7c8877f159f3c6f24934db5079290f606
Flekay <flekay@hotmail.de>
2026-02-12 08:19:58 +0100

    gamma improvements

--- Gamma ( γ )/python.py
+++ Gamma ( γ )/python.py
@@ -1,4 +1,7 @@
-from string import*
-r=0
-for c in"t1d0)n}ky/,/Z}:YHIjmRRVQz(ZPgiwAhn(p&$ro+yPYo0dXZ2(ziJ`>G+E_SZWl~jabR@5:|Ea1tb4uC]yX_s}'xn[4r(I]]}=9tRMu%uT9}-w$6w0)ei3xn&+ZD?&v5g6<JFw+@[$DhbN<x*BcP>PgB9I!7rmAq6t,VrSQqC{ CF'JRS0^!+QdTA?mmcqK5LG$Wk;Q-(LPRL2&kpNG3!z_bmqC0{F^q7/_1<Q|'}3PV/wpF@Hlj>2RFFR@i3_{{Ln8n)b*6t$lK%[ :whlre I4,KY))PJL]W1H=NLk>~hM;[P#/X[Rcp,%G^weX[A>jM_pF`0qHb #p!/YU59WfP/}<L?dkk`_[{sr?H:W?hn9~b7 ;#+Ch>,90H7n%z!DdEDTiUOMkaYa'3GC-|`93Q/B0NthI+{&Dn$E_0i=bo?~iV9Ebfb[J#F=~Tv2YR.1s%lF#Od@8{?[b%mIma7x6X=#wnP09oJ+&ga!!Hz4cBq{2`IU*Ss>;CLHU!X":r=r*93+{c:i for i,c in enumerate(digits+ascii_uppercase+ascii_lowercase+"!#$%&'()*+-.:;<=>?@[]^_{|}~`,/ ")}[c]
-print(f"0.{r}")
+from decimal import*
+getcontext().prec=a=1005
+e=Decimal(1)
+t=S=e
+h=T=e-e
+for k in range(1,a*3):t=t*a*a/k/k;h+=e/k;S+=t;T+=t*h
+print(str(T/S-(a*e).ln())[:-4])
